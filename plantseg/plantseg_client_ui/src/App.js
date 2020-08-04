c2945c6ca061d23dd9752ad9b6f57e47731dfc6c
B.I. Vodenicharski <biv20@cam.ac.uk>
2020-08-04 14:05:22 +0200

    Add React App folders

---
+++ plantseg/plantseg_client_ui/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// class PlantsegUI extends React.Component {
+//   render() {
+//     //TODO 
+//   }
+// }
+
+class ConfigsList extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      configFileNames: null,
+    }
+
+    console.log("fetching starts...")
+    fetch("127.0.0.1:8070/tasks")
+    .then(res => res.json())
+    .then((res) => this.setState({isLoading: false, configFileNames: res}))
+    .then(console.log("Fetching finished!"));
+  }
+
+  render() {
+
+    let message = this.state.isLoading ? "Loading..." : "Under construction!";
+
+    return <div>{message}</div>;
+  }
+}
+
+function App() {
+  console.log("App serve starting..")
+  return (
+    <ConfigsList />
+  );
+}
+
+export default App;
