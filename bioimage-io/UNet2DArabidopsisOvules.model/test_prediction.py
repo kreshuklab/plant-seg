cf06901f33a823c60c948561f20e529d87545f10
Adrian Wolny <adrian.wolny@iwr.uni-heidelberg.de>
2021-01-11 19:00:19 +0100

    Add bioimage model descriptors for Ovules networks (2d, 3d).

---
+++ bioimage-io/UNet2DArabidopsisOvules.model/test_prediction.py
@@ -0,0 +1,29 @@
+import numpy as np
+import torch
+from unet import UNet2D
+
+with torch.no_grad():
+    state = torch.load('unet2d-arabidopsis-ovules-confocal-ds2x.pytorch')
+
+    net = UNet2D(in_channels=1,
+                 out_channels=1,
+                 f_maps=64,
+                 testing=True)
+
+    net.load_state_dict(state)
+
+    # load and normalize the input
+    im = np.load('test_input.npz')
+    im = im['arr_0'].astype('float32')
+    im -= im.mean()
+    im /= im.std()
+
+    # forward pass
+    inp = torch.from_numpy(im)
+    out = net(inp)
+    out = out.cpu().numpy()
+
+    # compare with test_output
+    test_out = np.load('test_output.npz')
+    test_out = test_out['arr_0']
+    assert np.allclose(out, test_out)
