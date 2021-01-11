7c81a2de5451e8846258968a228c6b87e865722a
Adrian Wolny <adrian.wolny@iwr.uni-heidelberg.de>
2021-01-11 19:39:13 +0100

    Add bioimage model descriptors for Lateral Root Primordia network ds1x

---
+++ bioimage-io/UNet3DLateralRoot.model/test_prediction.py
@@ -0,0 +1,31 @@
+import numpy as np
+import torch
+from unet import UNet3D
+
+with torch.no_grad():
+    state = torch.load('unet3d-lateral-root-lightsheet-ds1x.pytorch')
+
+    net = UNet3D(in_channels=1,
+                 out_channels=1,
+                 f_maps=32,
+                 testing=True)
+
+    net.load_state_dict(state)
+
+    # load and normalize the input
+    im = np.load('test_input.npz')
+    im = im['arr_0'].astype('float32')
+    im -= im.mean()
+    im /= im.std()
+
+    # forward pass
+    inp = torch.from_numpy(im)
+    out = net(inp)
+    out = out.cpu().numpy()
+
+    np.savez('test_output.npz', out)
+
+    # compare with test_output
+    test_out = np.load('test_output.npz')
+    test_out = test_out['arr_0']
+    assert np.allclose(out, test_out)
---
+++ bioimage-io/UNet3DLateralRoot.model/unet3d.md
@@ -0,0 +1,6 @@
+# UNet3D Lateral Root Primordia
+
+A a variant of 3D U-Net trained to predict the cell boundaries in lightsheet stacks of Arabidopsis lateral root primordia.
+Voxel size: (0.25x0.1625x0.1625µm^3) (ZYX). 
+
+Trained on 3D lightsheet image stacks published [here](https://elifesciences.org/articles/57613).
