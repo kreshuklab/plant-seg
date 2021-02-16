8f72d8db66a12aae8a1e78cba33d6883f742107b
lorenzocerrone <lorenzo.cerrone@iwr.uni-heidelberg.de>
2021-02-16 09:50:05 +0100

    minor refactors and default homogenize

--- plantseg/pipeline/config_validation.py
+++ plantseg/pipeline/config_validation.py
@@ -19,66 +19,67 @@ def _error_message(error, key, value, fallback):
         gui_logger.warning(f"{_error}. defaulting default value: {fallback}")
 
 
-def is_string(key, value, fallback=None):
-    if not isinstance(value, str):
-        _error_message("value must be a string", key, value, fallback)
+def _is_type(key, value, fallback=None, check_types=(str,), text="string"):
+    if not isinstance(value, check_types):
+        _error_message(f"value must be a {text}", key, value, fallback)
         return fallback
     else:
         return value
+
+
+def is_string(key, value, fallback=None):
+    return _is_type(key, value, fallback=fallback, check_types=str, text="string")
 
 
 def is_float(key, value, fallback=None):
-    if not isinstance(value, (float, int)):
-        _error_message("value must be a float", key, value, fallback)
-        return fallback
-    else:
-        return value
+    return _is_type(key, value, fallback=fallback, check_types=(float, int), text="float (or integer)")
 
 
 def is_int(key, value, fallback=None):
-    if isinstance(value, str):
-        # some of the gui int come as strings
-        value = int(value)
-
-    if not isinstance(value, int):
-        _error_message("value must be a int", key, value, fallback)
-        return fallback
-    else:
-        return value
+    # some of the gui int come as strings
+    value = int(value) if isinstance(value, str) else value
+    return _is_type(key, value, fallback=fallback, check_types=int, text="integer")
 
 
 def is_binary(key, value, fallback=None):
-    if not isinstance(value, bool):
-        _error_message("value must be a bool", key, value, fallback)
+    return _is_type(key, value, fallback=fallback, check_types=bool, text="bool")
+
+
+def is_list(key, value, fallback=None):
+    return _is_type(key, value, fallback=fallback, check_types=(list, tuple), text="list (or tuple)")
+
+
+def is_length3(key, value, fallback=None):
+    if len(value) == 3:
+        _error_message(f"value must be a list of length 3", key, value, fallback)
         return fallback
     else:
         return value
 
 
-def is_3float_tuple(key, value, fallback=None):
-    if not isinstance(value, (list, tuple)) and len(value) == 3:
-        _error_message("value must be a list of length 3", key, value, fallback)
+def iterative_is_float(key, value, fallback=None):
+    for v in value:
+        return is_float(key, v, fallback)
+
+
+def iterative_is_int(key, value, fallback=None):
+    for v in value:
+        return is_int(key, v, fallback)
+
+
+def is_stride(key, value, fallback):
+    _options = [STRIDE_DRAFT, STRIDE_BALANCED, STRIDE_ACCURATE]
+    if isinstance(value, (tuple, list)):
+        value = is_length3(key, value, fallback)
+        value = iterative_is_int(key, value, fallback)
+        return value
+
+    elif value not in _options:
+        _error_message(f"value must be one of {_options} or a length 3 list of integers", key, value, fallback)
         return fallback
 
-    for v in value:
-        if not isinstance(v, (float, int)):
-            _error_message("value elements should be float or int", key, value, fallback)
-            return fallback
-
-    return value
-
-
-def is_3int_tuple(key, value, fallback=None):
-    if not isinstance(value, (list, tuple)) and len(value) == 3:
-        _error_message("value must be a list of length 3", key, value, fallback)
-        return fallback
-
-    for v in value:
-        if not isinstance(v, int):
-            _error_message("value elements should be int", key, value, fallback)
-            return fallback
-
-    return value
+    else:
+        return value
 
 
 def filter_name(key, value, fallback=None):
@@ -107,22 +108,11 @@ def model_exist(key, value, fallback):
         return value
 
 
-def is_cuda(key, value, fallback):
+def check_cuda(key, value, fallback):
     if value == 'cuda' and (not torch.cuda.is_available()):
         _error_message(f"torch can not detect a valid cuda device", key, value, fallback)
         return fallback
     return value
-
-
-def is_stride(key, value, fallback):
-    _options = [STRIDE_DRAFT, STRIDE_BALANCED, STRIDE_ACCURATE]
-    if isinstance(value, (tuple, list)):
-        return is_3int_tuple(key, value, fallback)
-    elif value not in _options:
-        _error_message(f"value must be one of {_options} or a length 3 list of integers", key, value, fallback)
-        return fallback
-    else:
-        return value
 
 
 def is_segmentation(key, value, fallback):
@@ -143,13 +133,14 @@ def is_0to1(key, value, fallback):
 
 class Check(object):
     def __init__(self, node):
+        assert 'tests' in node.keys(), 'Test is not configured correctly tests key is missing'
         check_list = []
         for test in node['tests']:
             m = importlib.import_module('plantseg.pipeline.config_validation')
             check_list.append(getattr(m, test))
 
         self.check_list = check_list
-        self.fallback = node['fallback']
+        self.fallback = node.get('fallback', None)
 
     def __call__(self, key, value):
         out = value
