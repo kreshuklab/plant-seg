9b424a95fac85c65cc62c5d1a734d07df8205717
lorenzocerrone <lorenzo.cerrone@iwr.uni-heidelberg.de>
2024-05-31 21:12:47 +0200

    fix doc ci

--- plantseg/viewer/viewer.py
+++ plantseg/viewer/viewer.py
@@ -10,17 +10,18 @@ from plantseg.viewer.widget.proofreading.proofreading import setup_proofreading_
 def run_viewer():
     viewer = napari.Viewer()
     main_container = get_data_io()
-    main_w = viewer.window.add_dock_widget(main_container, name='Data')
+    main_w = viewer.window.add_dock_widget(main_container, name="Data")
 
     setup_proofreading_keybindings(viewer)
 
-    for _containers, name in [(get_preprocessing_workflow(), 'Preprocessing'),
-                              (get_gasp_workflow(), 'Main'),
-                              (get_extra(), 'Extra'),
-                              (get_proofreading(), 'Proofreading')]:
+    for _containers, name in [
+        (get_preprocessing_workflow(), "Data-Processing"),
+        (get_gasp_workflow(), "Main"),
+        (get_extra(), "Extra"),
+        (get_proofreading(), "Proofreading"),
+    ]:
         _container_w = viewer.window.add_dock_widget(_containers, name=name)
         viewer.window._qt_window.tabifyDockWidget(main_w, _container_w)
 
-    napari_formatted_logging('Plantseg is ready!',
-                             thread='run_viewer', level='info')
+    napari_formatted_logging("Plantseg is ready!", thread="run_viewer", level="info")
     napari.run()
