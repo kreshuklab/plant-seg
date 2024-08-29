cc7a2a157c9c6a4a04f9bcf842322e7017a46124
Qin Yu <qin.yu.95@outlook.com>
2024-08-29 20:19:14 +0200

    fix(test-zoo): update use of `torch.load()`

--- tests/test_bioimageio.py
+++ tests/test_bioimageio.py
@@ -9,7 +9,7 @@ def test_get_3D_model_by_id():
     Load Qin Yu's nuclear segmentation model 'efficient-chipmunk'.
     """
     model, _, model_path = model_zoo.get_model_by_id('efficient-chipmunk')
-    state = torch.load(model_path, map_location='cpu')
+    state = torch.load(model_path, map_location='cpu', weights_only=True)
     if 'model_state_dict' in state:  # Model weights format may vary between versions
         state = state['model_state_dict']
     model.load_state_dict(state)
@@ -21,7 +21,7 @@ def test_get_2D_model_by_id():
     Load Adrian's 2D cell-wall segmentation model 'pioneering-rhino'.
     """
     model, _, model_path = model_zoo.get_model_by_id('pioneering-rhino')
-    state = torch.load(model_path, map_location='cpu')
+    state = torch.load(model_path, map_location='cpu', weights_only=True)
     if 'model_state_dict' in state:  # Model weights format may vary between versions
         state = state['model_state_dict']
     model.load_state_dict(state)
