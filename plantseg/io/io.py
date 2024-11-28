75d03675998054cc0bdd0e1252a15478414425b9
Qin Yu <qin.yu.95@outlook.com>
2024-11-28 19:05:53 +0100

    fix(io): image file suffixes in capital letter aren't recognised

--- plantseg/io/io.py
+++ plantseg/io/io.py
@@ -32,7 +32,7 @@ def smart_load(path: Path, key: str | None = None, default=load_tiff) -> np.ndar
         >>> data = smart_load('path/to/file.h5', key='raw')
 
     """
-    ext = path.suffix
+    ext = (path.suffix).lower()
     if key == "":
         key = None
 
@@ -72,7 +72,7 @@ def smart_load_with_vs(path: Path, key: str | None = None, default=load_tiff) ->
         >>> data = smart_load('path/to/file.h5', key='raw')
 
     """
-    ext = path.suffix
+    ext = (path.suffix).lower()
     if key == "":
         key = None
 
