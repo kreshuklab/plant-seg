eaf05e44cd443d794cf6bfb656c4932d4e8384d7
Adam Basha <55552142+adambasha0@users.noreply.github.com>
2025-01-24 15:43:01 +0100

    fix(tests): fix tests for napari widgets, fix #374 (#376)
    * Fix: Refactor test_widget_open_file to use qtbot for consistent GUI testing
    
    * style(pre-commit.ci): auto fixes
    
    * fix(tests): fix tests for napari widgets, fix #374
    
    Napari widget tests hang before
    
    ---------
    
    Co-authored-by: adambasha0 <readam@adbasha>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Qin Yu <qin.yu.95@outlook.com>

--- tests/widgets/test_widget_preprocessing.py
+++ tests/widgets/test_widget_preprocessing.py
@@ -1,6 +1,3 @@
-import os
-
-import napari
 import numpy as np
 import pytest
 from magicgui import magicgui
@@ -9,8 +6,6 @@ from napari.types import LayerDataTuple
 from plantseg.core.image import ImageLayout, ImageProperties, PlantSegImage, SemanticType
 from plantseg.io.voxelsize import VoxelSize
 from plantseg.viewer_napari.widgets.dataprocessing import RescaleModes, widget_rescaling
-
-IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"  # set to true in GitHub Actions by default to skip CUDA tests
 
 
 def create_layer_name(name: str, suffix: str):
@@ -51,38 +46,39 @@ def widget_add_image(image: PlantSegImage) -> LayerDataTuple:
     return image.to_napari_layer_tuple()
 
 
-@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="GUI tests hangs in GitHub Actions.")
 class TestWidgetRescaling:
-    def test_rescaling_from_factor(self, make_napari_viewer_proxy, sample_image):
+    def test_rescaling_from_factor(self, qtbot, make_napari_viewer_proxy, sample_image):
         viewer = make_napari_viewer_proxy()
         widget_add_image(sample_image)
 
         factor = 0.5
+        count_layers = len(viewer.layers)
         widget_rescaling(
             image=viewer.layers[sample_image.name],
             mode=RescaleModes.FROM_FACTOR,
             rescaling_factor=(factor, factor, factor),
             update_other_widgets=False,
         )
-        napari.run()
+        qtbot.waitUntil(lambda: count_layers < len(viewer.layers), timeout=20000)
 
         old_layer = viewer.layers[sample_image.name]
         new_layer = viewer.layers[sample_image.name + '_rescaled']
         np.testing.assert_allclose(new_layer.data.shape, np.multiply(old_layer.data.shape, factor), rtol=1e-5)
         np.testing.assert_allclose(np.multiply(new_layer.scale, factor), old_layer.scale, rtol=1e-5)
 
-    def test_rescaling_to_layer_voxel_size(self, make_napari_viewer_proxy, sample_image, sample_label):
+    def test_rescaling_to_layer_voxel_size(self, qtbot, make_napari_viewer_proxy, sample_image, sample_label):
         viewer = make_napari_viewer_proxy()
         widget_add_image(sample_image)
         widget_add_image(sample_label)
 
+        count_layers = len(viewer.layers)
         widget_rescaling(
             image=viewer.layers[sample_image.name],
             mode=RescaleModes.TO_LAYER_VOXEL_SIZE,
             reference_layer=viewer.layers[sample_label.name],
             update_other_widgets=False,
         )
-        napari.run()
+        qtbot.waitUntil(lambda: count_layers < len(viewer.layers), timeout=20000)
 
         reference_layer = viewer.layers[sample_label.name]
         new_layer = viewer.layers[sample_image.name + '_rescaled']
@@ -90,17 +86,18 @@ class TestWidgetRescaling:
         np.testing.assert_allclose(new_layer.data.shape, reference_layer.data.shape, rtol=1e-5)
         np.testing.assert_allclose(new_layer.scale, reference_layer.scale, rtol=1e-5)
 
-    def test_rescaling_to_model_voxel_size(self, make_napari_viewer_proxy, sample_image):
+    def test_rescaling_to_model_voxel_size(self, qtbot, make_napari_viewer_proxy, sample_image):
         viewer = make_napari_viewer_proxy()
         widget_add_image(sample_image)
 
+        count_layers = len(viewer.layers)
         widget_rescaling(
             image=viewer.layers[sample_image.name],
             mode=RescaleModes.TO_MODEL_VOXEL_SIZE,
             reference_model='PlantSeg_3Dnuc_platinum',  # voxel size: (0.2837, 0.1268, 0.1268)
             update_other_widgets=False,
         )
-        napari.run()
+        qtbot.waitUntil(lambda: count_layers < len(viewer.layers), timeout=20000)
 
         old_layer = viewer.layers[sample_image.name]
         new_layer = viewer.layers[sample_image.name + '_rescaled']
@@ -116,18 +113,19 @@ class TestWidgetRescaling:
 
         np.testing.assert_allclose(new_shape * new_scale, old_shape * old_scale, atol=0.1)
 
-    def test_rescaling_to_voxel_size(self, make_napari_viewer_proxy, sample_image):
+    def test_rescaling_to_voxel_size(self, qtbot, make_napari_viewer_proxy, sample_image):
         viewer = make_napari_viewer_proxy()
         widget_add_image(sample_image)
 
         expected_scale = (0.5, 0.5, 0.5)  # target voxel size
+        count_layers = len(viewer.layers)
         widget_rescaling(
             image=viewer.layers[sample_image.name],
             mode=RescaleModes.TO_VOXEL_SIZE,
             out_voxel_size=expected_scale,
             update_other_widgets=False,
         )
-        napari.run()
+        qtbot.waitUntil(lambda: count_layers < len(viewer.layers), timeout=20000)
 
         old_layer = viewer.layers[sample_image.name]
         new_layer = viewer.layers[sample_image.name + '_rescaled']
@@ -138,18 +136,19 @@ class TestWidgetRescaling:
         np.testing.assert_allclose(new_layer.data.shape, expected_shape, rtol=1e-5)
         np.testing.assert_allclose(new_layer.scale, expected_scale, rtol=1e-5)
 
-    def test_rescaling_to_layer_shape(self, make_napari_viewer_proxy, sample_image, sample_label):
+    def test_rescaling_to_layer_shape(self, qtbot, make_napari_viewer_proxy, sample_image, sample_label):
         viewer = make_napari_viewer_proxy()
         widget_add_image(sample_image)
         widget_add_image(sample_label)
 
+        count_layers = len(viewer.layers)
         widget_rescaling(
             image=viewer.layers[sample_image.name],
             mode=RescaleModes.TO_LAYER_SHAPE,
             reference_layer=viewer.layers[sample_label.name],
             update_other_widgets=False,
         )
-        napari.run()
+        qtbot.waitUntil(lambda: count_layers < len(viewer.layers), timeout=20000)
 
         reference_layer = viewer.layers[sample_label.name]
         new_layer = viewer.layers[sample_image.name + '_reshaped']
@@ -159,18 +158,19 @@ class TestWidgetRescaling:
         # In our special case in this test, yes, the scale should be the same
         np.testing.assert_allclose(new_layer.scale, reference_layer.scale, rtol=1e-5)
 
-    def test_rescaling_set_shape(self, make_napari_viewer_proxy, sample_image):
+    def test_rescaling_set_shape(self, qtbot, make_napari_viewer_proxy, sample_image):
         viewer = make_napari_viewer_proxy()
         widget_add_image(sample_image)
 
         target_shape = (20, 50, 50)
+        count_layers = len(viewer.layers)
         widget_rescaling(
             image=viewer.layers[sample_image.name],
             mode=RescaleModes.TO_SHAPE,
             reference_shape=target_shape,
             update_other_widgets=False,
         )
-        napari.run()
+        qtbot.waitUntil(lambda: count_layers < len(viewer.layers), timeout=20000)
 
         old_layer = viewer.layers[sample_image.name]
         new_layer = viewer.layers[sample_image.name + '_reshaped']
@@ -182,18 +182,19 @@ class TestWidgetRescaling:
 
         np.testing.assert_allclose(new_layer.scale, expected_scale, rtol=1e-5)
 
-    def test_rescaling_set_voxel_size(self, make_napari_viewer_proxy, sample_image):
+    def test_rescaling_set_voxel_size(self, qtbot, make_napari_viewer_proxy, sample_image):
         viewer = make_napari_viewer_proxy()
         widget_add_image(sample_image)
 
         target_voxel_size = (2.0, 2.0, 2.0)
+        count_layers = len(viewer.layers)
         widget_rescaling(
             image=viewer.layers[sample_image.name],
             mode=RescaleModes.SET_VOXEL_SIZE,
             out_voxel_size=target_voxel_size,
             update_other_widgets=False,
         )
-        napari.run()
+        qtbot.waitUntil(lambda: count_layers < len(viewer.layers), timeout=20000)
 
         new_layer = viewer.layers[sample_image.name + '_set_voxel_size']
         np.testing.assert_allclose(new_layer.scale, target_voxel_size, rtol=1e-5)
