fa79945a7ed29f5332020f97d6ab01c84fa0979b
Qin Yu <qin.yu.95@outlook.com>
2025-01-31 01:50:20 +0100

    refactor: algorithm and readability of patch size finder

--- plantseg/functionals/prediction/utils/size_finder.py
+++ plantseg/functionals/prediction/utils/size_finder.py
@@ -22,16 +22,32 @@ def find_patch_and_halo_shapes(
     both_sides: bool = False,
 ) -> tuple[tuple[int, int, int], tuple[int, int, int]]:
     """
-    Recommend patch shape and halo size for a given 3D sample shape.
+    Recommend an optimal patch shape and halo size for a given 3D sample. Since convolving
+    kernels are often isotropic, adjusting and fitting an initially isotropic maximum patch
+    shape to the full sample image helps achieve a more balanced patch shape while ensuring
+    optimal hardware utilization and accurate predictions.
 
     Args:
-        full_volume_shape (tuple[int, int, int]): Shape of the entire 3D sample.
-        max_patch_shape (tuple[int, int, int]): Maximum feasible patch shape for the selected GPU.
-        min_halo_shape (tuple[int, int, int]): Minimum halo size, counting one side by default.
-        both_sides (bool, optional): Whether the halo size is counted on both sides. Defaults to False.
+        full_volume_shape (tuple[int, int, int]):
+            The shape of the full 3D volume (Z, Y, X).
+        max_patch_shape (tuple[int, int, int]):
+            The maximum feasible patch shape for the GPU (Z, Y, X).
+        min_halo_shape (tuple[int, int, int]):
+            The minimum required halo size per side (Z, Y, X).
+        both_sides (bool, optional):
+            If True, the `min_halo_shape` is applied symmetrically on both sides of the patch.
+            Defaults to False.
 
     Returns:
-        tuple[tuple[int, int, int], tuple[int, int, int]]: Recommended patch shape and 1-side halo shape.
+        tuple[tuple[int, int, int], tuple[int, int, int]]:
+            - Recommended patch shape `(Z, Y, X)`.
+            - Halo size on one side `(Z, Y, X)`.
+
+    Notes:
+        - If the total voxel capacity of `max_patch_shape` is larger than or equal to the entire
+          volume, we simply return the full volume shape with zero halo.
+        - Otherwise, we find which dimensions actually require tiling and set a minimum halo
+          there (0 otherwise).
     """
     shape_volume = np.array(full_volume_shape)
     shape_patch_max = np.array(max_patch_shape)
@@ -42,27 +58,25 @@ def find_patch_and_halo_shapes(
 
     if n_voxels_patch >= n_voxels_sample:
         return tuple(shape_volume), (0, 0, 0)
+    else:  # otherwise at least one dim needs to be tiled
+        dim_shrink = shape_patch_max > shape_volume
+        halo_shape = np.where(dim_shrink, 0, shape_halo_min)
+        count_shrink = dim_shrink.sum()
 
-    # Adjust patch shape if necessary
-    shrink = shape_patch_max > shape_volume
-    adjusted_patch_shape = np.minimum(shape_volume, shape_patch_max)
-
-    if np.any(shrink):
-        n_shrink_dims = np.sum(shrink)
-        if n_shrink_dims == 2:
-            remaining_dim = np.flatnonzero(~shrink)
-            adjusted_patch_shape[remaining_dim] = min(
-                n_voxels_patch // np.prod(shape_volume[shrink]), shape_volume[remaining_dim]
-            )
-        elif n_shrink_dims == 1:
-            remaining_dims = np.flatnonzero(~shrink)
-            max_area = n_voxels_patch // shape_volume[shrink]
-            if max_area <= np.prod(shape_volume[remaining_dims]):
-                adjusted_patch_shape[remaining_dims] = np.sqrt(max_area).astype(int)
-            else:
-                return tuple(shape_volume), (0, 0, 0)
-    halo_shape = np.where(shrink, 0, shape_halo_min)
-    return tuple(adjusted_patch_shape - halo_shape * 2), tuple(halo_shape)
+        adjusted_patch_shape = np.minimum(shape_volume, shape_patch_max)
+        if count_shrink == 3:
+            pass  # adjusted_patch_shape is already shape_volume
+        elif count_shrink == 2:
+            remaining_dim = np.flatnonzero(~dim_shrink)
+            adjusted_patch_shape[remaining_dim] = n_voxels_patch // np.prod(shape_volume[dim_shrink])
+        elif count_shrink == 1:
+            remaining_dims = np.flatnonzero(~dim_shrink)
+            max_area = n_voxels_patch // shape_volume[dim_shrink]
+            adjusted_patch_shape[remaining_dims] = np.sqrt(max_area).astype(int)
+        else:  # count_shrink == 0
+            assert np.all(shape_patch_max <= shape_volume)
+            adjusted_patch_shape = shape_patch_max
+        return tuple(adjusted_patch_shape - halo_shape * 2), tuple(halo_shape)
 
 
 def find_a_max_patch_shape(
