5629b03f90be01a18a909f61545106e68006f710
KRiedmiller <42350211+KRiedmiller@users.noreply.github.com>
2025-04-17 12:09:48 +0200

    fix: gpu added

--- constructor_win/win_installer.bat
+++ constructor_win/win_installer.bat
@@ -1,12 +1,13 @@
 call "%PREFIX%\Scripts\activate.bat"
 cd "%PREFIX%"
+set CONDA_OVERRIDE_CUDA="12.4"
 tar xf win_build.gz
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-conda create -y -n plantseg python=3.12
+conda create -y -n plantseg python=3.12 menuinst
 conda activate plantseg
 echo 'Activated, now installing Plantseg..'
-conda install -c "%PREFIX%\conda_bld" -c conda-forge plantseg
+conda install -v -c "%PREFIX%\conda_bld" -c conda-forge plantseg
 echo 'Installation finished!'
 touch 'plantseg_installed'
 conda info
