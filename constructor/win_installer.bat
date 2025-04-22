733c57b7aefb1d785eee06faebb5a69f1e9833a5
KRiedmiller <42350211+KRiedmiller@users.noreply.github.com>
2025-04-22 10:44:12 +0200

    fix: macos installer added, final testing

--- constructor/win_installer.bat
+++ constructor/win_installer.bat
@@ -1,6 +1,6 @@
 echo "### This is the main installation of PlantSeg ###"
 CALL "%PREFIX%\Scripts\activate.bat"
 cd "%PREFIX%"
-tar xf win_build.gz
+tar xf build.gz
 CALL conda install -y -c "%PREFIX%\conda_bld" -c conda-forge plantseg
 echo 'Installation finished!'
