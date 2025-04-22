f44b39c55e2c9a0a230fe365af8770efa4e70813
KRiedmiller <42350211+KRiedmiller@users.noreply.github.com>
2025-04-22 14:01:21 +0200

    fix: cleanup

--- constructor/unix_installer.sh
+++ constructor/unix_installer.sh
@@ -1,6 +1,6 @@
 echo "### This is the main installation of PlantSeg ###"
-. "$PREFIX/etc/profile.d/conda.sh" && conda activate "$PREFIX"
-cd "$PREFIX"
+. "${PREFIX}/etc/profile.d/conda.sh" && conda activate "${PREFIX}"
+cd "${PREFIX}"
 tar xf build.gz
 conda install -y -c "${PREFIX}/conda_bld" -c conda-forge plantseg
 echo "Installation finished!"
--- constructor_win/win_installer.bat
+++
@@ -1,6 +0,0 @@
-echo "### This is the main installation of PlantSeg ###"
-CALL "%PREFIX%\Scripts\activate.bat"
-cd "%PREFIX%"
-tar xf win_build.gz
-CALL conda install -y -c "%PREFIX%\conda_bld" -c conda-forge plantseg
-echo 'Installation finished!'
