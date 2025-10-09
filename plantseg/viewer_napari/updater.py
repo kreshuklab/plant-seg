02bf6ceeabf30acb86676b543711c2ccd66fcbf0
bump-my-version <bump-my-version@github.actions>
2025-10-09 12:55:40 +0200

    fix: replace deprecated conda api

--- plantseg/viewer_napari/updater.py
+++ plantseg/viewer_napari/updater.py
@@ -1,25 +1,20 @@
 from napari.qt.threading import thread_worker
 
 from plantseg.viewer_napari import log
+import subprocess
 
 
 def update():
-    try:
-        import conda.cli.python_api as api
-        from conda.exceptions import PackageNotInstalledError
-    except ModuleNotFoundError:
-        log(
-            "Conda not found! Please update manually!",
-            thread="updater",
-            level="WARNING",
-        )
-        return
-
     @thread_worker
     def _update():
         try:
-            api.run_command("update", "plant-seg")
-        except PackageNotInstalledError:
+            subprocess.run(
+                ["conda", "update", "plant-seg"],
+                input="y\n",
+                text=True,
+                check=True,
+            )
+        except subprocess.CalledProcessError:
             log(
                 "Unable to update! If you have installed via git, please update your local repo!",
                 thread="updater",
@@ -29,5 +24,9 @@ def update():
 
         log("Update finished, please restart!", thread="updater", level="INFO")
 
-    log("Starting update, GUI might freeze!", thread="updater", level="INFO")
+    log(
+        "Starting update, might take a while!\nCheck the progress in the terminal.",
+        thread="updater",
+        level="INFO",
+    )
     _update().start()
