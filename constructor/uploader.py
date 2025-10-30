9f44280305d51181abfce58e84c18fd9492476ab
bump-my-version <bump-my-version@github.actions>
2025-10-30 11:50:18 +0100

    chore: attach installer to release

--- constructor/uploader.py
+++ constructor/uploader.py
@@ -1,7 +1,8 @@
 import sys
 from pathlib import Path
-
+import subprocess
 import requests
+import json
 
 assert len(sys.argv) == 2, "Uploader needs heibox api token"
 heibox_token = sys.argv[1]
@@ -14,6 +15,7 @@ upload_url = response.text.strip('"')
 assert len(upload_url) > 50, f"Failed to get upload url: {upload_url}"
 print("Obtained upload url")
 
+to_upload = None
 for to_upload in Path("installer").glob("PlantSeg*"):
     print("Uploading ", to_upload)
     with open(to_upload, "rb") as fp:
@@ -24,3 +26,24 @@ for to_upload in Path("installer").glob("PlantSeg*"):
         response = requests.post(upload_url, data=payload, files=files, headers=headers)
 
 print("Uploaded installer, ", response.text)
+
+# attach to release
+if to_upload:
+    output = subprocess.run(
+        ["gh", "release", "list", "-L", "1", "--json", "tagName"],
+        capture_output=True,
+        text=True,
+    ).stdout
+    tag = json.loads(output)[0]["tagName"]
+    up_out = subprocess.run(
+        ["gh", "release", "upload", tag, f"installer/{to_upload.name}"],
+        capture_output=True,
+        text=True,
+    )
+    print(
+        f"Attached installer {to_upload.name} to Release {tag}!\n"
+        f"Output: {up_out.stdout}, {up_out.stderr}"
+    )
+else:
+    print("Could not upload anything!")
+    print(list(Path("installer").glob("PlantSeg*")))
